6bc83cc0fd4b10c947ee959007f6dfe914862fc9
aalmfer2001 <123627142+aalmfer2001@users.noreply.github.com>
2023-02-16 10:24:36 +0100

    Add files via upload

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,3 +3,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 . ~/.bashrc
 nvm install --lts
+ln -sf "SNVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+ln -sf "SNVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
+mkdir -p /home/ubuntu/app/release/logs
